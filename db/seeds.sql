9f46c1fd2b1416f31e38614858519ae8a5282420
Elia Carrasco <eliafacundoorta@gmail.com>
2019-12-18 23:15:06 -0600

    updated schema, seeds, and server.js files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Whiskey King Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Company Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Original Burger');
