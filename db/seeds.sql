cefb9197148fd4f40959d6def521cb21b6a1909e
Elia Carrasco <eliafacundoorta@gmail.com>
2020-03-14 20:45:51 -0500

    working code

--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (burger_name) VALUES ('Whiskey King Burger');
-INSERT INTO burgers (burger_name) VALUES ('The Company Burger');
-INSERT INTO burgers (burger_name) VALUES ('The Original Burger');
--- server.js
+++ server.js
@@ -1,33 +1,23 @@
 var express = require("express");
-var exphbs = require("express-handlebars");
-var mysql = require("mysql");
+
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+app.use(express.static("public"));
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "efMAY17!",
-  database: "burger_db"
-});
+var routes = require("./controllers/burger_controller.js");
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+app.use(routes);
 
-  console.log("connected as id " + connection.threadId);
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
 });
